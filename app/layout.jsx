539587c8d16c2a2f2570b899b35c13ea97382650
JuliaPasquier <pasquierjulia5@gmail.com>
2024-02-22 16:29:19 +0100

    first commit

---
+++ app/layout.jsx
@@ -0,0 +1,24 @@
+import Navbar from '@/components/Navbar';
+import Footer from '@/components/Footer';
+
+import '@/assets/globals.css';
+
+export const metadata = {
+    title: 'Projet Next.js | Find The Perfect Rental',
+    description: 'Find your dream rental property',
+    keywords: 'rental, find rentals, find properties',
+}
+
+const MainLayout = ({ children }) => {
+  return (
+    <html lang='en'>
+    <body>
+      <Navbar />
+      <main>{children}</main>
+      <Footer />
+    </body>
+  </html>
+  );
+};
+
+export default MainLayout;
