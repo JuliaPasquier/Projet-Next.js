b0c801610f4d68f1a78a4e81a3bb6fab9f34e5bd
JuliaPasquier <pasquierjulia5@gmail.com>
2024-02-23 13:25:14 +0100

    fetch data

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,10 @@
 import Hero from '@/components/Hero';
 import InfoBoxes from '@/components/InfoBoxes';
 import HomeProperties from '@/components/HomeProperties';
+import connectDB from '@/config/database';  
 
-const HomePage = () => {
-    console.log(process.env.MONGODB_URI)
+
+const HomePage =  () => {
     return (
         <>
         <Hero />
--- app/properties/page.jsx
+++ app/properties/page.jsx
@@ -1,24 +1,44 @@
-import PropertyCard from '@/components/PropertyCard'; 
-import properties from '@/properties.json';
-   
+import PropertyCard from '@/components/PropertyCard';
 
-const PropertiesPage = () => {
-    return (
-        <section className='px-4 py-6'>
-            <div className='container-xl lg:container m-auto px-4 py-6'>
-                {properties.length === 0 ? (
-                    <p>No properties found</p>
-                ) : (
-                    <div className='grid grid-cols-1 md:grid-cols-3 gap-6'>
-                        {properties.map((property) => (
-                            <PropertyCard key={property._id} property=
-                             {property}/>
-                        ))}
-                    </div>
-                )}
-            </div>
-        </section>
-    );
+async function fetchProperties() {
+  try {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_API_DOMAIN}/
+    properties`);
+
+
+    if (!res.ok) {
+        throw new Error("Failed to fetch data");
+    }
+
+    return res.json();
+
+  } catch (error) {
+    console.error(error);
+  }
 }
+
+
+
+const PropertiesPage = async () => {
+    const properties = await fetchProperties();
+
+    console.log(properties);
+
+  return (
+    <section className="px-4 py-6">
+      <div className="container-xl lg:container m-auto px-4 py-6">
+        {properties && properties.length === 0 ? (
+          <p>No properties found</p>
+        ) : (
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+            {properties && properties.map((property) => (
+              <PropertyCard key={property._id} property={property} />
+            ))}
+          </div>
+        )}
+      </div>
+    </section>
+  );
+};
 
 export default PropertiesPage;
