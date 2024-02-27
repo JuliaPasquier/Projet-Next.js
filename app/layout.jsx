89f4151fdb02c9aab715599619e002389e711c61
JuliaPasquier <pasquierjulia5@gmail.com>
2024-02-27 11:24:01 +0100

    auth google

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,24 +1,28 @@
-import Navbar from '@/components/Navbar';
-import Footer from '@/components/Footer';
+import "@/assets/styles/tailwind.css";
 
-import '@/assets/globals.css';
+import AuthProvider from '@/components/AuthProvider';
+import Footer from '@/components/Footer';
+import Navbar from '@/components/Navbar';
+
 
 export const metadata = {
-    title: 'Projet Next.js | Find The Perfect Rental',
-    description: 'Find your dream rental property',
-    keywords: 'rental, find rentals, find properties',
-}
+    title: "PropertyPulse | Find The Perfect Rental",
+    description:
+        "Find your dream rental property with PropertyPulse. We have a wide range of properties to choose from.",
+};
 
 const MainLayout = ({ children }) => {
-  return (
-    <html lang='en'>
-    <body>
-      <Navbar />
-      <main>{children}</main>
-      <Footer />
-    </body>
-  </html>
-  );
+    return (
+        <AuthProvider>
+            <html lang="en">
+                <body>
+                    <Navbar />
+                    <main>{children}</main>
+                    <Footer />
+                </body>
+            </html>
+        </AuthProvider>
+    );
 };
 
 export default MainLayout;
